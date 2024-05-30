914f9124523382933bf0eed7bf65721af63c359a
ASproson <a.sproson@outlook.com>
2024-05-30 18:54:23 +0100

    Base React-Tailwind install

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Test } from "./components/Test";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello test</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,7 @@
+export const Test = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello world</h1>
+    </div>
+  );
+};
